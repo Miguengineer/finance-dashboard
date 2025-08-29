e4cdd54aaae76e73cc51cc98e764a9b634fb7c53
Miguengineer <durandiazmiguel@gmail.com>
2025-08-29 12:09:16 -0400

    Updated page

--- firebase-config.js
+++ firebase-config.js
@@ -8,4 +8,15 @@
 //   messagingSenderId: "1234567890",
 //   appId: "1:1234567890:web:abcdefg"
 // };
-export const firebaseConfig = {};
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhiGkgLk-h9yc93yChtMO7V7yaD4i9QEU",
+  authDomain: "financedashboardjm.firebaseapp.com",
+  projectId: "financedashboardjm",
+  storageBucket: "financedashboardjm.firebasestorage.app",
+  messagingSenderId: "152763496566",
+  appId: "1:152763496566:web:c556aa91fbfb6fdd4d8757"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
