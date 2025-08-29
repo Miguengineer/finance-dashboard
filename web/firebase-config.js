cd28aa8f03e07ac892a67ca960b9d5bfe0aa6a23
Miguengineer <durandiazmiguel@gmail.com>
2025-08-29 11:49:29 -0400

    Init web

---
+++ web/firebase-config.js
@@ -0,0 +1,11 @@
+// Fill this with your Firebase project Web config (see README).
+// Example:
+// export const firebaseConfig = {
+//   apiKey: "AIza...",
+//   authDomain: "your-project.firebaseapp.com",
+//   projectId: "your-project",
+//   storageBucket: "your-project.appspot.com",
+//   messagingSenderId: "1234567890",
+//   appId: "1:1234567890:web:abcdefg"
+// };
+export const firebaseConfig = {};
